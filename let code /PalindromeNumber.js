dfa18403448da50fb993c1122df54f892aceafe4
mohelsadany <mohelsadany@gmail.com>
2022-04-21 10:38:26 +0200

    restrcure

--- let code /PalindromeNumber.js
+++ let code /PalindromeNumber.js
@@ -23,6 +23,8 @@ var isPalindrome = function (x) {
   return x == x.toString().split('').reverse().join('');
 };
 
+console.log(isPalindrome(-121))
+
 // ************************************** //
 
 
