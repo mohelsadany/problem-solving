c927b86f344a4f94d8b0530ccde6be6c6bacf3c3
mohelsadany <mohelsadany@gmail.com>
2022-04-22 00:11:39 +0200

    adding vaild parentheses

--- let code /longestCommonPrefix.js
+++ let code /longestCommonPrefix.js
@@ -17,3 +17,40 @@ var longestCommonPrefix = function(strs) {
 
 console.log(longestCommonPrefix(["flower","flow","flight"]));
 console.log(longestCommonPrefix(["dog","racecar","car"]));
+
+var longestCommonPrefix = function (strs) {
+    // Return early on empty input
+    if (!strs.length) return '';
+
+    // Loop through the letters of the first string
+    for (let i = 0; i <= strs[0].length; i++) {
+        // Loop through the other strings
+        for (let j = 1; j < strs.length; j++) {
+            // Check if this character is also present in the same position of each string
+            if (strs[0][i] !== strs[j][i]) {
+                // If not, return the string up to and including the previous character
+                return strs[0].slice(0, i);
+            }
+        }
+    }
+
+    return strs[0];
+};
+
+
+
+var longestCommonPrefix = function (strs) {
+    // Return early on empty input
+    if (!strs.length) return '';
+
+    // Loop through the letters of the first word
+    for (let i = 0; i <= strs[0].length; i++) {
+        // Check if this character is present in the same position of every string
+        if (!strs.every((string) => string[i] === strs[0][i])) {
+            // If not, return the string up to and including the previous character
+            return strs[0].slice(0, i);
+        }
+    }
+
+    return strs[0];
+};
---
+++ let code /validParentheses.js
@@ -0,0 +1,52 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+ var isValid = function (inputString) {
+    // Initialise an array to store the closing brackets expected
+    let expectedBrackets = [];
+
+    // Loop through the letters in the input string
+    for (let letter = 0; letter < inputString.length; letter++) {
+        // Push the closing equivelant of any open brackets found
+        if (inputString[letter] == '{') {
+            expectedBrackets.push('}');
+        } else if (inputString[letter] == '[') {
+            expectedBrackets.push(']');
+        } else if (inputString[letter] == '(') {
+            expectedBrackets.push(')');
+        }
+        // If a close bracket is found, check that it matches the last stored open bracket
+        else if (expectedBrackets.pop() !== inputString[letter]) {
+            return false;
+        }
+    }
+
+    // Return based on whether or not anything remains
+    // (indicates that there were incorrect brackets)
+    return !expectedBrackets.length;
+};
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+ var isValid = function(s) {
+    var stack = [];
+    var len = s.length;
+    var map = {
+      '(': ')',
+      '[': ']',
+      '{': '}'
+    };
+    for (var i = 0; i < len; i++) {
+      if (stack.length > 0 && map[stack[stack.length - 1]] === s[i]) {
+        stack.pop();
+      } else {
+        stack.push(s[i]);
+      }
+    }
+    return stack.length === 0;
+  };
+
+  console.log(isValid("()"));
