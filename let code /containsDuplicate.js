7b360dc2268064e22e09d208e0354bc89b4da6a5
mohelsadany <mohelsadany@gmail.com>
2022-05-06 00:22:05 +0200

    enhnacements

--- let code /containsDuplicate.js
+++ let code /containsDuplicate.js
@@ -1,7 +1,4 @@
-/**
- * @param {number[]} nums
- * @return {boolean}
- */
+
 var containsDuplicate = function(nums) {
     const map={};
     for(const num of nums){
--- let code /implement-strstr.js
+++ let code /implement-strstr.js
@@ -1,8 +1,4 @@
-/**
- * @param {string} haystack
- * @param {string} needle
- * @return {number}
- */
+
 var strStr = function (haystack, needle) {
   if (needle.length === 0) return 0;
 
