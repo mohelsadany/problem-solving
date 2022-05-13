b91a8182ce51ceef7a9aad24409ee84defa79f2b
mohelsadany <mohelsadany@gmail.com>
2022-05-13 16:18:06 +0200

    inorderTraversal

---
+++ let code /inorderTraversal.js
@@ -0,0 +1,15 @@
+var inorderTraversal = function (root) {
+  let stack = [];
+  let res = [];
+
+  while (root !== null || stack.length !== 0) {
+    while (root !== null) {
+      stack.push(root);
+      root = root.left;
+    }
+    root = stack.pop();
+    res.push(root.val);
+    root = root.right;
+  }
+  return res;
+};
