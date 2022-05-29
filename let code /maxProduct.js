3dcf9f86c0fbe9415dc6d52710d5869ab337c26e
mohelsadany <mohelsadany@gmail.com>
2022-05-30 01:06:36 +0200

    maxProduct using js

---
+++ let code /maxProduct.js
@@ -0,0 +1,17 @@
+//Maximum Product of Word Lengths  https://leetcode.com/problems/maximum-product-of-word-lengths/
+var maxProduct = function(words) {
+    words.sort((a,b) => b.length - a.length)
+    let best = 0, bitsets = new Uint32Array(words.length)
+    for (let i = 0; i < words.length; i++) {
+        let word = words[i], wlen = word.length, bitset = 0
+        if (wlen * words[0].length < best)
+            return best
+        for (let j = 0; j < wlen; j++)
+            bitset |= 1 << (word.charCodeAt(j) - 97)
+        for (let j = 0; j < i; j++)
+            if ((bitsets[j] & bitset) === 0)
+                best = Math.max(best, wlen * words[j].length)
+        bitsets[i] = bitset
+    }
+    return best
+};
