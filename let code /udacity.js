996dce97c53ee8b507fb046a2722a2ef151e4172
mohelsadany <mohelsadany@gmail.com>
2022-06-16 03:28:57 +0200

    udicity soltion

---
+++ let code /udacity.js
@@ -0,0 +1,74 @@
+// var x =3;
+// var y ="";
+// var z =4;
+// if(x==y){
+//     z ++
+// }
+// console.log(z);
+
+// var went = true;
+// var going = went ? "yes" : "no";
+// var x = "yes";
+// if(going!="no"){
+//     x += 1+"no";
+// }
+// console.log(going);
+
+// a = [1,2,3];
+// b=[3];
+// c= a*b;
+// console.log(c);
+
+// function makeApPromise(resolve,reject){
+//     resolve(1);
+//     setTimeout(resolve,2000,2);
+// }
+// let promise = new Promise(makeApPromise);
+// console.log(promise);
+
+// function email(){
+//     return new Promise((resolve)=>{
+//         setTimeout(()=>{
+//             resolve("welcome user 1t");
+//         },2000);
+//     });
+// }
+
+// async function emailmsg(){  
+//     const msg = await email();
+//     console.log("Message send",msg);
+// }
+// emailmsg();
+
+
+// const button = document.querySelector("button");
+// button.addEventListener("click",event=>{
+//     waitfunctio();
+// });
+// async function waitfunctio(){
+//     setTimeout('alertbox()',2000);
+// }
+// function alertbox(){
+//     console.log("hello");
+// }
+
+// var I;
+// var y =6;
+// var x= "ooooo" ;
+// for(I=0;I<x.length;I++){
+//    {y+=1;}
+// }  
+// console.log(y);
+
+// var a =5;
+// var b =6;
+
+// function sum(a,b){
+//     console.log(a+b);
+// }
+// console.log(sum(a,b));
+
+
+
+
+
